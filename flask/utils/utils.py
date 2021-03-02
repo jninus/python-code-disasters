d07e602e0838255b4d7e45f766ca656f0d3da8d3
reda zejli <reda.zejli@gmail.com>
2021-03-02 20:30:03 +0100

    Amélioration code pour requeter la base de données une seule fois

--- flask/utils/utils.py
+++ flask/utils/utils.py
@@ -6,4 +6,9 @@ SOCIAL_QUESTIONS_IDS = [1, 2, 15, 16, 17]
 
 
 def get_social_questions():
-    return [db.session.query(Question).get(q) for q in SOCIAL_QUESTIONS_IDS]
+ #   return [db.session.query(Question).get(q) for q in SOCIAL_QUESTIONS_IDS]
+
+     # Presuming id is the primary key
+     # Retrieving from an sql request is better than looping throw array
+    return db.session.query(Question).filter(Question.id.in_(SOCIAL_QUESTIONS_IDS)).all()
+
